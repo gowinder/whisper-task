7d8e513d34b326fc98fa720a9e5b5f5870e87e08
destroyer <destroyer@shipyard.xyz>
2023-05-26 10:47:26 +0800

    功能(frontend): add color theme

--- vite-frontend/src/App.tsx
+++ vite-frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { SetStateAction, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import AppBar from './components/AppBar';
 import viteLogo from '/vite.svg';
 import './index.css';
 
 function App() {
+  const [theme, setTheme] = useState('');
+
+  const handleThemeChange = (newTheme: string) => {
+    console.log('App handleThemeChange', newTheme);
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="bg-main flex flex-col bg-base-500">
-      <AppBar />
+    <div className={`${theme} flex flex-col bg-base`}>
+      <AppBar onThemeChange={handleThemeChange} />
     </div>
   );
 }
--- vite-frontend/src/components/AppBar.tsx
+++ vite-frontend/src/components/AppBar.tsx
@@ -2,8 +2,15 @@ import { Menu, Popover } from '@headlessui/react';
 import React from 'react';
 import { BsList, BsSliders } from 'react-icons/bs';
 
-export default function AppBar() {
+export default function AppBar({ onThemeChange }) {
   const [showSettings, setShowSettings] = React.useState(false);
+  const [selectedTheme, setSelectedTheme] = React.useState('');
+
+  const handleThemeChange = (event) => {
+    const newTheme = event.target.value;
+    setSelectedTheme(newTheme);
+    onThemeChange(newTheme);
+  };
 
   return (
     <div className="flex flex-col">
@@ -14,13 +21,15 @@ export default function AppBar() {
               <BsList className="btn-app-icon" />
             </Popover.Button>
             <Popover.Panel className="absolute z-10">
-              <div className="flex flex-col p-4 rounded-lg shadow hover:ring-1 text-slate-100 bg-base-600">
-                <div>
-                  <p>d3</p>
-                </div>
-                <div>
-                  <p>b3</p>
-                </div>
+              <div className="flex flex-col p-4 divide-y rounded-md divide-primaryfront shadow hover:ring-1 text-slate-100 bg-neutral">
+                <ul>
+                  <li>
+                    <p>abc</p>
+                  </li>
+                  <li>
+                    <p>Stop all whisper tasks</p>
+                  </li>
+                </ul>
               </div>
             </Popover.Panel>
           </Popover>
@@ -28,9 +37,20 @@ export default function AppBar() {
         <div className="grow text-center">
           <h1 className="text-3xl text-slate-200">Whisper Task</h1>
         </div>
-        <button className="btn btn-app">
-          <BsSliders className="btn-app-icon" />
-        </button>
+        <div className="flex justify-center items-center space-x-2">
+          <label className="text-basefront">Theme</label>
+          <select
+            className="rounded-md bg-primary shadow-md text-primaryfront"
+            value={selectedTheme}
+            onChange={handleThemeChange}>
+            <option value="">default</option>
+            <option value="theme-green">green</option>
+            <option value="theme-purple">purple</option>
+          </select>
+          <button className="btn btn-app">
+            <BsSliders className="btn-app-icon" />
+          </button>
+        </div>
       </div>
       <div>
         <span>abc</span>
